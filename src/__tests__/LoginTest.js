b0c69370fe1f8a2dece3617549d5223b1eb46576
bhavani125 <girinenibhavani12@gmil.com>
2021-12-24 15:34:06 +0530

    refactored code

--- src/__tests__/LoginTest.js
+++ src/__tests__/LoginTest.js
@@ -1,5 +1,5 @@
-import Enzyme,{ shallow} from 'enzyme'
-import Adapter  from '@wojtekmaj/enzyme-adapter-react-17'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 
 
 import LoginIn from "../Components/Login/Login";
@@ -11,7 +11,7 @@ const shallowMountLogin = shallow(<LoginIn />);
 
 describe('test loginpage is loading Successfully ', () => {
 
-    it("test if loginpage  is laoded", () => {
+	it("test if loginpage  is laoded", () => {
 		expect(shallowMountLogin.find('.LoginContainer').exists()).toBe(true);
 	});
 
@@ -23,15 +23,15 @@ describe('test loginpage is loading Successfully ', () => {
 		expect(shallowMountLogin.find('#outlined-basic').exists()).toBe(true);
 	});
 
-    it("test if loginpage  is laoded", () => {
+	it("test if loginpage  is laoded", () => {
 		expect(shallowMountLogin.find('.LoginContainers').exists()).toBe(false);
 	});
 
-	it('test if email is loading', () => {
-		expect(shallowMountLogin.find('.outlined-basic').exists()).toBe(false);
-	});
 
 	it('test if password is loading', () => {
+		expect(shallowMountLogin.find('.outlined-basic').exists()).toBe(false);
+	});
+	it('test if email is loading', () => {
 		expect(shallowMountLogin.find('.outlined-basic').exists()).toBe(false);
 	});
 
